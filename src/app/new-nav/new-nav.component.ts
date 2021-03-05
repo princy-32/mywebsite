49d1b987c1a822190bb7601f8748cb87134128d9
princy-32 <51436953+princy-32@users.noreply.github.com>
2021-03-05 12:38:24 +0530

    commit

---
+++ src/app/new-nav/new-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-nav',
+  templateUrl: './new-nav.component.html',
+  styleUrls: ['./new-nav.component.css']
+})
+export class NewNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
